d6ab486a6e764c9f798e90ce35a86dab9274c1f2
Henrique Wildes <henriquewildes@gmail.com>
2017-01-27 02:05:28 -0200

    Adicionando Algoritmo de Compressão e Descompressão

--- project/public/js/controllers/links-controller.js
+++ project/public/js/controllers/links-controller.js
@@ -12,16 +12,6 @@
 
 	firebase.initializeApp(config);
 
-	// var links = [{"id": "23094", "hits": 1003, "url": "http://globo.com", "shortUrl": "http://chr.dc/9dtr4"}, 
- //        {"id": "76291", "hits": 1922, "url": "http://google.com", "shortUrl": "http://chr.dc/aUx71"}, 
- //        {"id": "66761", "hits": 765, "url": "http://terra.com.br", "shortUrl": "http://chr.dc/u9jh3"}, 
- //        {"id": "70001", "hits": 1519, "url": "http://facebook.com", "shortUrl": "http://chr.dc/qy61p"}, 
- //        {"id": "21220", "hits": 311, "url": "http://diariocatarinense.com.br", "shortUrl": "http://chr.dc/87itr"}, 
- //        {"id": "10743", "hits": 722, "url": "http://uol.com.br", "shortUrl": "http://chr.dc/y81xc"}, 
- //        {"id": "19122", "hits": 1320, "url": "http://chaordic.com.br", "shortUrl": "http://chr.dc/qy5k9"}, 
- //        {"id": "55324", "hits": 997, "url": "http://youtube.com", "shortUrl": "http://chr.dc/1w5tg"}, 
- //        {"id": "70931", "hits": 487, "url": "http://twitter.com", "shortUrl": "http://chr.dc/7tmv1"}, 
- //        {"id": "87112", "hits": 130, "url": "http://bing.com", "shortUrl": "http://chr.dc/9opw2"}];
 
 	angular.module('chaordic', ['firebase'])
 	.controller('LinksController', function($scope, $firebaseArray) {
@@ -29,9 +19,7 @@
 		var linksRef = firebase.database().ref('links');
 
 		$scope.listarLinks = function() {
-            // console.log(links);
-            // $scope.links = links;
-
+            
             // Adiciona ouvinte no grupo Links do Firebase
             linksRef.on('value', function() {
   				$scope.links = $firebaseArray(linksRef);
@@ -47,7 +35,7 @@
                 id : timestamp.toString(),
                 hits : 0,
                 url : $scope.url,
-                shortUrl : "teste.com"
+                shortUrl : idToShortLink(timestamp)
             };
 
             // Enviar novo Link p/ Firebase
@@ -59,12 +47,51 @@
             .catch(function(error) {
             	console.log("Registro não sucedido.");
             	console.log(error);
-            })
-
-            // links.push(novoLink);
-            // $scope.listarLinks();
+            });
         };
 
+
+        // As funções a seguir foram adaptadas do seguinte site de 
+        // referência: http://www.geeksforgeeks.org/how-to-design-a-tiny-url-or-url-shortener/
+
+        // Esta função se resume em converter o ID, anteriormente determinado pelo timestamp em que o link foi criado,
+        // da base 10 para a base 62 (a quantidade de caracteres possíveis entre a-z + A-Z e 0-9), resultando numa
+        // URL compactada.
+        function idToShortLink(id) {
+
+		    var shortLink = [];
+
+		    var mapa = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+		    
+		    while (id > 0) {
+		        shortLink.push(mapa[id % 62]);
+		        id = Math.floor(id / 62);
+		    }
+
+		    shortLink.reverse();
+		    return shortLink.join("");
+		}
+
+		// Esta função retorna o ID de um Link após descompactar a URL, tratando de uma conversão, dessa vez,
+		// da base 62 para a base 10.
+		function shortLinkToId(shortLink) {
+
+		    var id = 0;
+		  
+		    for (var i = 0; i < shortLink.length; i++) {
+		        if ('a' <= shortLink[i] && shortLink[i] <= 'z' ) {
+		            id = id * 62 + shortLink[i].charCodeAt(0) - 'a'.charCodeAt(0);
+		        }
+		        if ('A' <= shortLink[i] && shortLink[i] <= 'Z' ) {
+		            id = id * 62 + shortLink[i].charCodeAt(0) - 'A'.charCodeAt(0) + 26;
+		        }
+		        if ('0' <= shortLink[i] && shortLink[i] <= '9' ) {
+		            id = id * 62 + shortLink[i].charCodeAt(0) - '0'.charCodeAt(0) + 52;
+		        }
+		    }
+
+		    return id;
+		}
 	});
 
 
